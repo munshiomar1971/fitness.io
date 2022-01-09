9c6398c8fe302384f056f66ecd8b7de53f332b91
Omar Sumon <73730920+worstfrontendcoder@users.noreply.github.com>
2022-01-08 21:03:33 -0600

    basic set up

---
+++ website/function.js
@@ -0,0 +1,12 @@
+// fitness website
+
+// TODO:
+// - create main page
+// - create excersize page
+// - create exercise generator page
+// - create button to generate exercises
+// - create array of exercises
+// - create function to generate random exercises
+// - create function to display exercises
+// - style everything with css
+
